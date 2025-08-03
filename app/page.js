c0d3a5e7a51f085dbc9263ead58d10d6a3b7c94d
Abel Nutsugah <bigmanabel@outlook.com>
2025-08-02 20:59:18 -0700

    Refactor Home component structure: simplify headings, enhance layout with cards for user statistics and app performance

--- app/page.js
+++ app/page.js
@@ -3,21 +3,60 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="p-20">
-      <h1 className="text-3xl/[140%] text-[#2E2A2A] font-[600] ">Overview</h1>
+      <h1>Overview</h1>
 
-      <h3 className="text-[20px] text-[#000000] font-[600]">Users</h3>
-      {/* New Sign-Ups */}
-      {/* Active Users */}
-      {/* Inactive Users */}
-      <h3 className="text-[20px] text-[#000000] font-[600]">App Performance</h3>
-      {/* Average Response Time */}
-      {/* Error Rate */}
-      {/* Uptime */}
-      <h3 className="text-[20px] text-[#000000] font-[600]">System Health</h3>
-      {/* System Usage */}
-      {/* Memory Usage */}
-      {/* Server Usage By Country */}
-      {/* Server Usage By State */}
-  </main>
+      <div className="my-[63px] p-[1px]">
+        <h3 className="-mt-[28px]">Users</h3>
+        <div className="flex w-full gap-4">
+          {/* New Sign-Ups */}
+          <div className="card flex-1">
+            New Sign-Ups
+          </div>
+          {/* Active Users */}
+          <div className="card flex-1">
+            Active Users
+          </div>
+          {/* Inactive Users */}
+          <div className="card flex-1">
+            Inactive Users
+          </div>
+        </div>
+        <h3>App Performance</h3>
+        <div className="flex w-full gap-4">
+          {/* Average Response Time */}
+          <div className="card flex-1">
+            Average Response Time
+          </div>
+          {/* Error Rate */}
+          <div className="card flex-1">
+            Error Rate
+          </div>
+          {/* Uptime */}
+          <div className="card flex-1">
+            Uptime
+          </div>
+        </div>
+
+        <h3 className="pt-[67px]">System Health</h3>
+        <div className="flex w-full gap-4 mb-[40px]">
+          {/* System Usage */}
+          <div className="card flex-1">
+            System Usage
+          </div>
+          {/* Memory Usage */}
+          <div className="card flex-1">
+            Memory Usage
+          </div>
+        </div>
+        {/* Server Usage By Country */}
+        <div className="card w-full mb-[40px]">
+          Server Usage By Country
+        </div>
+        {/* Server Usage By State */}
+        <div className="card w-full mb-[40px]">
+          Server Usage By State
+        </div>
+      </div>
+    </main>
   );
 }
